229900ad5c8f000dd02b836d5ce32f2c9c01081b
devongillis <devon.gillis@ucalgary.ca>
2019-01-24 12:49:52 -0700

    here

---
+++ aboutus/devon_gillis.js
@@ -0,0 +1,7 @@
+document.write("Devon Gillis 10126480, " 
++ "I am a fourth year engineering student and "
++ "am taking this course as it is a requirement of my program. "
++ "I hope to gain an even deeper understanding of web development "
++ "and am looking forward to this semester. "
++ "My interests include coding, puzzles(especially rubix cubes), and drawing."
+);
