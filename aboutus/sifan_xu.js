484a9ddf5564533a5a4424ee46e6b53fb752ddfa
sifanxu1996 <sifan.xua@gmail.com>
2019-01-24 12:50:22 -0700

    aboutus update

--- aboutus/sifan_xu.js
+++ aboutus/sifan_xu.js
@@ -1,1 +1,6 @@
-document.write("");
+document.write("" 
++ "I am a 3rd year computer science student at the U of C "
++ "and I've always had a passion for learning and programming. "
++ "I have participated in various interesting projects in the past "
++ "and look forward to new experiences with new people. "
++ "My interests include reading, writing, learning programming and drawing.");
