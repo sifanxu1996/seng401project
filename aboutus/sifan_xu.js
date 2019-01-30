d66547a84ecde66a3db4894ae37a2e0554712d14
sifanxu1996 <sifan.xua@gmail.com>
2019-01-29 23:56:39 -0700

    Update sifan_xu.js

--- aboutus/sifan_xu.js
+++ aboutus/sifan_xu.js
@@ -1,4 +1,5 @@
-document.write("" 
+document.write(""
++ "UCID: 10146334 <br>"
 + "I am a 3rd year computer science student at the U of C "
 + "and I've always had a passion for learning and programming. "
 + "I have participated in various interesting projects in the past "
