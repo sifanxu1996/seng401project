5799889f62ca28a37c602e60966da38c02eba1f6
ali_hassam <alykhanhssm@gmail.com>
2019-01-30 23:14:35 -0700

    Update ali_hassam.js

--- aboutus/ali_hassam.js
+++ aboutus/ali_hassam.js
@@ -1,1 +1,3 @@
-document.write("i");
+document.write("My name is Alykhan Hassam, but my friends call me Ali."
+			  +"I'm in my 3rd year of software engineering." 
+			  +"I'm looking forward to learning more about web development in this course");
