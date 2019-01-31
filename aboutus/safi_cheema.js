8b049e76e5a986da40e406f7bf0805459b126f71
safi cheema <saficheema@gmail.com>
2019-01-31 06:19:50 -0700

    Add: Personal Picture and descripation
    Add Personal picture and descripation

--- aboutus/safi_cheema.js
+++ aboutus/safi_cheema.js
@@ -1,1 +1,4 @@
-document.write("");
+document.write("Safi Cheema 10128903. " 
+	+"I am in my third year of software engineering at University of Calgary."
+	+"I really enjoy coding and programming and doing outdoor activities like hiking and camping."
+	);
